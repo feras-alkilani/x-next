19841f3ed887cf8139ef8cbaeee26b177127ab03
Feras Alkilani <eng.feras90@gmail.com>
2024-09-23 23:29:14 +0200

    Update the News component

--- src/app/components/News.jsx
+++ src/app/components/News.jsx
@@ -1,5 +1,45 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 
 export default function News() {
-  return <div>News</div>;
+  const [news, setNews] = useState([]);
+
+  const [articleNum, setArticleNum] = useState(3);
+
+  useEffect(() => {
+    fetch("https://saurav.tech/NewsAPI/everything/cnn.json")
+      .then((response) => response.json())
+      .then((data) => setNews(data.articles));
+  }, []);
+  return (
+    <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 ">
+      <h4 className="font-bold text-xl px-4">Whats happening</h4>
+      {news.slice(0, articleNum).map((article) => (
+        <div key={article.url}>
+          <a href={article.url} target="_blank">
+            <div className="flex items-center justify-between px-4 py-2 space-x-1 hover:bg-gray-200 transition duration-200">
+              <div className="space-y-0.5">
+                <h6 className="text-sm font-bold px-4">{article.title}</h6>
+                <p className="text-gray-500 px-4 text-xs font-medium">
+                  {article.source.name}
+                </p>
+              </div>{" "}
+              <img
+                src={article.urlToImage}
+                className=" rounded-xl"
+                width={70}
+              />
+            </div>
+          </a>
+        </div>
+      ))}
+      <button
+        onClick={() => setArticleNum((prev) => prev + 3)}
+        className="text-blue-300 pl-4 pb-3 hover:text-blue-400 text-sm"
+      >
+        Load more
+      </button>
+    </div>
+  );
 }
