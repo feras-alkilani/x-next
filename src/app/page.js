bab5154111b6d750023a4d9fa498b8d6a86f7c34
Feras Alkilani <eng.feras90@gmail.com>
2024-09-29 15:32:44 +0200

    Add input component and update the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,13 @@
 import React from "react";
+import Input from "./components/Input";
 
 export default function page() {
-  return <h1 className="text-red-500">Home</h1>;
+  return (
+    <div className="max-w-xl mx-auto border-r border-l min-h-screen">
+      <div className="py-2 px-3 sticky top-0 z-99 bg-white border-b border-gray-200">
+        <h2 className="text-lg sm:text-xl font-bold">Home</h2>
+      </div>
+      <Input />
+    </div>
+  );
 }
